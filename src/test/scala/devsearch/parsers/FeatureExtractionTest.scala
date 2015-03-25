cabf7f64c01d246da314f4a56a6cbf9032f5589d
Pierre Walch <pwalch@gmail.com>
2015-03-25 13:56:39 +0100

    Add some sample for feature extraction

--- src/test/scala/devsearch/parsers/FeatureExtractionTest.scala
+++ src/test/scala/devsearch/parsers/FeatureExtractionTest.scala
@@ -1,8 +1,24 @@
 package devsearch.parsers
 
+import devsearch.ast.AST
+import org.scalatest.FlatSpec
+
 /**
  * Created by pierre on 25/03/15.
  */
-class FeatureExtractionTest {
+class FeatureExtractionTest extends FlatSpec {
+    def getAst(): AST = {
+        val fileURL = getClass.getResource("/samples/AccountDataManager.java")
+        val filePath = new java.io.File(fileURL.toURI).getAbsolutePath
+        JavaParser.parse(filePath)
+    }
+
+    "Feature extractor" should "be blabla" in {
+        println(getAst())
+    }
+
+    it should "coucou" in  {
+
+    }
 
 }
