521f172ad3c9aeedb4f612353dc7df2e84654fc4
Pierre Walch <pwalch@gmail.com>
2015-03-27 12:56:19 +0100

    Add (wrong) test for import extractor

--- src/test/scala/devsearch/features/ImportExtractorTest.scala
+++ src/test/scala/devsearch/features/ImportExtractorTest.scala
@@ -1,8 +1,24 @@
 package devsearch.features
 
+import devsearch.parsers.JavaParser
+import org.scalatest.{Matchers, FlatSpec}
+import spray.json.JsonParser
+
 /**
  * Created by pierre on 27/03/15.
  */
-class ImportExtractorTest {
-
+class ImportExtractorTest extends FlatSpec with Matchers {
+    "import extractor" should "extract all imports" in {
+        val fileURL = getClass.getResource("/samples/JavaConcepts.java")
+        val filePath = new java.io.File(fileURL.toURI).getAbsolutePath
+        val ast = JavaParser.parse(filePath)
+        assert(ImportExtractor.extractFeatures(ast) == JsonParser( """[
+            {"type": "import", "domain": "com.github.javaparser.JavaParser", "containsAsterisk": "false", "isStatic": "false"},
+            {"type": "import", "domain": "japa.parser.ParseException", "containsAsterisk": "false", "isStatic": "false"},
+            {"type": "import", "domain": "com.github.javaparser.ast.CompilationUnit", "containsAsterisk": "false", "isStatic": "false"},
+            {"type": "import", "domain": "org.junit.Ignore", "containsAsterisk": "false", "isStatic": "false"},
+            {"type": "import", "domain": "java.io.*", "containsAsterisk": "true", "isStatic": "false"},
+            {"type": "import", "domain": "java.util.*", "containsAsterisk": "true", "isStatic": "false"}
+        ]"""))
+    }
 }
