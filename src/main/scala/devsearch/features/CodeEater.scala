e715c0ca5d332d261bc77b7b2e243eb794350909
Pierre Walch <pwalch@gmail.com>
2015-04-03 00:47:41 +0200

    Add doc in CodeEater for Hubi

--- src/main/scala/devsearch/features/CodeEater.scala
+++ src/main/scala/devsearch/features/CodeEater.scala
@@ -7,7 +7,13 @@ import org.apache.spark.rdd.RDD
 case class CodeFileLocation(user: String, repoName: String, fileName: String) extends Serializable
 case class CodeFileData(codeFileLocation: CodeFileLocation, syntaxTree: AST) extends Serializable
 
+/**
+ * Entry point for Spark feature mining script.
+ */
 object CodeEater {
+    /**
+     * Eats code and returns distinct features (no duplicates)
+     */
     def eat(sc: SparkContext, inputData: RDD[CodeFileData]): RDD[AbstractFeature] = {
         val emptyRDD: RDD[AbstractFeature] = sc.emptyRDD[AbstractFeature]
         List[AbstractFeatureExtractor](
