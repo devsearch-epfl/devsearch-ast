02af3a72d3d7f1aa046cf5adae10ee655974f804
Nicolas Voirol <voirol.nicolas@gmail.com>
2015-04-10 19:17:10 +0200

    Moved some classes for clarity

--- src/main/scala/devsearch/features/CodeEater.scala
+++ src/main/scala/devsearch/features/CodeEater.scala
@@ -4,34 +4,14 @@ import devsearch.ast._
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-case class CodeFileData(location: CodeFileLocation, ast: AST) extends java.io.Serializable
-
-case class CodeFileLocation(user: String, repoName: String, fileName: String) extends java.io.Serializable {
-  def at(pos: Position) = CodeFilePosition(this, pos.line)
-  def at(line: Int) = CodeFilePosition(this, line)
-}
-
-case class CodeFilePosition(location: CodeFileLocation, line: Int) extends java.io.Serializable
-
 /**
  * Entry point for Spark feature mining script.
  */
 object CodeEater {
-    /**
-     * Eats code and returns distinct features (no duplicates)
-     */
-    def eat(sc: SparkContext, inputData: RDD[CodeFileData]): RDD[Feature] = {
-        val emptyRDD: RDD[Feature] = sc.emptyRDD[Feature]
-        List[FeatureExtractor](
-            ImportFeatures,
-            InheritanceFeatures,
-            ValDefFeatures,
-            FunDefFeatures,
-            TypeFeatures,
-            StructuralFeatures
-        ).foldLeft(emptyRDD)(
-            (acc, extractor) =>
-              sc.union(acc, inputData.flatMap(data => extractor.extract(data)).distinct)
-        )
-    }
+  /**
+   * Eats code and returns distinct features (no duplicates)
+   */
+  def eat(sc: SparkContext, inputData: RDD[CodeFileData]): RDD[Feature] = {
+    inputData.flatMap(FeatureExtractor.extract)
+  }
 }
--- src/main/scala/devsearch/features/Features.scala
+++ src/main/scala/devsearch/features/Features.scala
@@ -1,5 +1,7 @@
 package devsearch.features
 
+import devsearch.ast._
+
 abstract class Feature(position: CodeFilePosition) extends java.io.Serializable {
   def key: String
 }
@@ -7,3 +9,27 @@ abstract class Feature(position: CodeFilePosition) extends java.io.Serializable 
 trait FeatureExtractor extends java.io.Serializable {
   def extract(data: CodeFileData): Traversable[Feature]
 }
+
+object FeatureExtractor extends java.io.Serializable {
+
+  lazy val extractors = List(
+    ImportFeatures,
+    InheritanceFeatures,
+    ValDefFeatures,
+    FunDefFeatures,
+    TypeFeatures,
+    StructuralFeatures
+  )
+
+  def extract(data: CodeFileData) = extractors.flatMap(_.extract(data))
+}
+
+case class CodeFileLocation(user: String, repoName: String, fileName: String) extends java.io.Serializable {
+  def at(pos: Position) = CodeFilePosition(this, pos.line)
+  def at(line: Int) = CodeFilePosition(this, line)
+}
+
+case class CodeFilePosition(location: CodeFileLocation, line: Int) extends java.io.Serializable
+
+case class CodeFileData(location: CodeFileLocation, ast: AST) extends java.io.Serializable
+
