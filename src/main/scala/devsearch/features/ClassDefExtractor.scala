25fe71475c697ee05458e4dd98dd002c60939fef
Pierre Walch <pwalch@gmail.com>
2015-04-23 14:47:06 +0200

    Ignore incorrect super classes

--- src/main/scala/devsearch/features/ClassDefExtractor.scala
+++ src/main/scala/devsearch/features/ClassDefExtractor.scala
@@ -13,10 +13,17 @@ case class InheritanceFeature(position: CodePiecePosition, className: String, su
 object ClassDefExtractor extends FeatureExtractor {
   def extract(data: CodeFileData): Set[Feature] = data.ast.collect[Feature] {
     case cd @ ClassDef(_, name, _, _, _, _, _) if name != Names.DEFAULT =>
-      Set(ClassNameFeature(data.location at cd.pos, name)) ++
-      cd.superClasses.collect { case ClassType(_, superClass, _, _) if superClass != Names.DEFAULT =>
-        InheritanceFeature(data.location at cd.pos, cd.name, superClass)
-      }.toSet
+      try {
+        Set(ClassNameFeature(data.location at cd.pos, name)) ++
+            cd.superClasses.collect { case ClassType(_, superClass, _, _) if superClass != Names.DEFAULT =>
+              InheritanceFeature(data.location at cd.pos, cd.name, superClass)
+            }.toSet
+      } catch {
+        case cce: ClassCastException =>
+          // println("Class cast exception at:" + (data.location at cd.pos))
+          Set.empty
+      }
+
     case _ => Set.empty
   }
 }
