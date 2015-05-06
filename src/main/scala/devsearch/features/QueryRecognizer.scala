977e88a12031dab103a3a82bf4d431b991019522
Pierre Walch <pwalch@gmail.com>
2015-05-06 19:06:05 +0200

    Add tweaks suggested by Nicolas in PR

--- src/main/scala/devsearch/features/QueryRecognizer.scala
+++ src/main/scala/devsearch/features/QueryRecognizer.scala
@@ -4,34 +4,30 @@ import devsearch.parsers.Languages
 
 object QueryRecognizer {
   def apply(query: String): Option[CodeFile] = {
-    scala.util.Try {
-      Languages.supportedLanguages().toList.map { language =>
-        // Some parsers (Go) need a valid file name to parse correctly, so we
-        // generate one for the query.
-        val generatedFileName = "file" + (Languages.extension(language) match {
-          case Some(extension) => "." + extension
-          case None => ""
-        })
+    Languages.supportedLanguages().toList.map { language =>
+      // Some parsers (Go) need a valid file name to parse correctly, so we
+      // generate one for the query.
+      val generatedFileName = "file" + (Languages.extension(language) match {
+        case Some(extension) => "." + extension
+        case None => ""
+      })
 
-        CodeFile(
-          language,
-          CodeFileLocation("dummy_user", "dummy_repo", generatedFileName),
-          query
-        )
-      }.map { codeFile =>
-        // Scala parser is more permissive, so we reduce its score for the comparison.
-        val weight: Double = codeFile.language match {
-          case Languages.Scala => 0.7
-          case _ => 1.0
-        }
-        val score: Double = FeatureRecognizer(codeFile).size.toDouble * weight
-        (codeFile, score)
-      }.sortBy(_._2)
-      .reverse.head match {
-        case (codeFile, score) if score > 0.0 => Some(codeFile)
-        case _ => None
+      CodeFile(
+        language,
+        CodeFileLocation("dummy_user", "dummy_repo", generatedFileName),
+        query
+      )
+    }.map { codeFile =>
+      // Scala parser is more permissive, so we reduce its score for the comparison.
+      val weight: Double = codeFile.language match {
+        case Languages.Scala => 0.7
+        case _ => 1.0
       }
-      // head can throw NoSuchElementException
-    }.getOrElse(None)
+      val score: Double = FeatureRecognizer(codeFile).size.toDouble * weight
+      (codeFile, score)
+    }.sortBy(-_._2).headOption match {
+      case Some((codeFile, score)) if score > 0.0 => Some(codeFile)
+      case _ => None
+    }
   }
 }
