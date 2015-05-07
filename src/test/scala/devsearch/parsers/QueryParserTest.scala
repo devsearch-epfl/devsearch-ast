2aa9d8caf44d2a6336e7edd64c1cb073e6918d3e
Nicolas Voirol <voirol.nicolas@gmail.com>
2015-05-07 09:16:53 +0200

    Split QueryParser into scala and query parser.

--- src/test/scala/devsearch/parsers/QueryParserTest.scala
+++ src/test/scala/devsearch/parsers/QueryParserTest.scala
@@ -7,142 +7,33 @@ import devsearch.ast.Modifiers._
 
 class QueryParserTest extends FlatSpec {
 
-  "Query language" should "be parsable" in {
-    val source = new ContentsSource("Test.scala", """
-      def a = 2
-      def $_ = 1
-      def _ = 1
+  def testParser(code: String): Unit = {
+    val source = new ContentsSource("Test.scala", code)
 
-      def toto(_) = 1
-
-      case class Toto(a: Int, b: Int)
-      val t @ Toto(a,b) = Toto(1,2)
-
-      for (i <- List.range(1,2)) yield i
-
-      for (i <- List.range(1,2);
-           a = 2;
-           _ <- toto;
-           e <- List.range(3,4)) yield i + e + a
-    """)
-
+    // just make sure it doesn't crash
     QueryParser.parse(source)
+
+    // make sure this one crashes!
+    assert(try {
+      val ast = ScalaParser.parse(source)
+      println(ast)
+      false
+    } catch {
+      case t: ParsingFailedError =>
+        true
+    })
   }
 
-  it should "parse for comprehensions" in {
-    val source = new ContentsSource("NoFile", """
-      for (i <- List.range(1,2);
-           a = 2;
-           _ <- toto;
-           e <- List.range(3,4)) yield i + e + a
-    """)
-
-    assert(QueryParser.parse(source) == Foreach(
-      List(ValDef(FINAL,"i",List(),NoType,NoExpr,false)),
-      FunctionCall(
-        FieldAccess(Ident("List"),"range",List()),List(),List(
-          SimpleLiteral(PrimitiveTypes.Int,"1"),
-          SimpleLiteral(PrimitiveTypes.Int,"2")
-        )),Block(List(
-          ValDef(FINAL,"a",List(),NoType,SimpleLiteral(PrimitiveTypes.Int,"2"),false),
-          Foreach(
-            List(ValDef(FINAL,"_",List(),NoType,NoExpr,false)),
-            Ident("toto"),
-            Foreach(
-              List(ValDef(FINAL,"e",List(),NoType,NoExpr,false)),
-              FunctionCall(FieldAccess(Ident("List"),"range",List()),List(),List(
-                SimpleLiteral(PrimitiveTypes.Int,"3"),
-                SimpleLiteral(PrimitiveTypes.Int,"4")
-              )), BinaryOp(BinaryOp(Ident("i"),"+",Ident("e")),"+",Ident("a")),true),true))),true))
+  "Query language" should "work for empty names" in {
+    testParser("def _ = 1")
   }
 
-  it should "parse varArgs" in {
-    val source = new ContentsSource("NoFile", """
-      def test(a: String*)
-    """)
-
-    assert(QueryParser.parse(source) == FunctionDef(FINAL | ABSTRACT,"test",List(),List(),
-      List(ValDef(FINAL,"a",List(),PrimitiveTypes.String,NoExpr,true)),
-      PrimitiveTypes.Void, NoStmt))
+  it should "work for argument holes" in {
+    testParser("def toto(_) = 1")
   }
 
-  it should "parse primitive types" in {
-    val source = new ContentsSource("NoFile", """
-      val a = 1
-      val b: Int = 2
-      def toto(f: Int => Int) = f(2)
-    """)
-
-    assert(QueryParser.parse(source) == Block(List(
-      ValDef(FINAL,"a",List(),NoType,SimpleLiteral(PrimitiveTypes.Int,"1"),false),
-      ValDef(FINAL,"b",List(),PrimitiveTypes.Int,SimpleLiteral(PrimitiveTypes.Int,"2"),false),
-      FunctionDef(FINAL,"toto",List(),List(),List(
-        ValDef(FINAL,"f",List(),FunctionType(List(PrimitiveTypes.Int), PrimitiveTypes.Int), NoExpr,false)
-      ),NoType,Block(List(
-        FunctionCall(Ident("f"),List(),List(SimpleLiteral(PrimitiveTypes.Int,"2")))))),
-      VoidLiteral)))
-  }
-
-  it should "parse null literal" in {
-    val source = new ContentsSource("NoFile", "val a = null")
-
-    assert(QueryParser.parse(source) == ValDef(FINAL,"a",List(),NoType,NullLiteral,false))
-  }
-
-  it should "parse loops" in {
-    val source = new ContentsSource("NoFile", """
-      while (a < 0) {
-        a += 1
-        println(a)
-      }
-    """)
-
-    assert(QueryParser.parse(source) == While(
-      BinaryOp(Ident("a"),"<",SimpleLiteral(PrimitiveTypes.Int,"0")),
-      Block(List(
-        Assign(Ident("a"),SimpleLiteral(PrimitiveTypes.Int,"1"), Some("+")),
-        FunctionCall(Ident("println"),List(),List(Ident("a")))))))
-  }
-
-  it should "parse expression loops" in {
-    val source = new ContentsSource("NoFile", """
-      val a = while(b < 0) {
-        b += 1
-      }
-    """)
-
-    assert(QueryParser.parse(source) == ValDef(FINAL,"a",List(),NoType,Block(List(
-      While(BinaryOp(Ident("b"),"<",SimpleLiteral(PrimitiveTypes.Int,"0")),
-        Block(List(Assign(Ident("b"),SimpleLiteral(PrimitiveTypes.Int,"1"),Some("+"))))),
-      VoidLiteral)),false))
-  }
-
-  it should "parse do-while statements" in {
-    val source = new ContentsSource("NoFile", """
-      do {
-        println(a.next)
-      } while (a.hasNext)
-    """)
-
-    assert(QueryParser.parse(source) == Do(FieldAccess(Ident("a"),"hasNext",List()),
-      Block(List(FunctionCall(Ident("println"),List(),List(FieldAccess(Ident("a"),"next",List())))))))
-  }
-
-  it should "work for named arguments" in {
-    val source = new ContentsSource("NoFile", "test(a = 1, b = 2)")
-
-    assert(QueryParser.parse(source) == FunctionCall(Ident("test"),List(),List(
-      Assign(Ident("a"),SimpleLiteral(PrimitiveTypes.Int,"1"),None),
-      Assign(Ident("b"),SimpleLiteral(PrimitiveTypes.Int,"2"),None))))
-  }
-
-  it should "work for annotations" in {
-    val source = new ContentsSource("NoFile", "@annot(a = 1, b = 2) val a = 1")
-
-    assert(QueryParser.parse(source) == ValDef(FINAL,"a",List(Annotation("annot",Map(
-        "a" -> SimpleLiteral(PrimitiveTypes.Int,"1"),
-        "b" -> SimpleLiteral(PrimitiveTypes.Int,"2")
-      ))),NoType,SimpleLiteral(PrimitiveTypes.Int,"1"),false))
+  it should "work for for-loop holes" in {
+    testParser("for (i <- List.range(1,2); _) yield i")
   }
 
   it should "be robust" in {
